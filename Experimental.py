68a14d3abaa5783c9a855e2efda65ff1d5bdcc82
minkrov <minkrov@proton.me>
2026-02-24 11:52:41 +0100

    2D lists and Nested Loops in Experimental.py

--- Experimental.py
+++ Experimental.py
@@ -4,3 +4,14 @@ number = input("Enter the number you want to power: ")
 power = input("Enter the power you want to raise the number to: ")
 
 print(int(number) ** int(power))
+
+# 2D Lists and Nested Loops
+
+number_grid = [
+	[1, 2, 3],
+	[4, 5, 6],
+	[7, 8, 9,]
+]
+
+# print(number_grid[row][column])
+print(number_grid[0][2])

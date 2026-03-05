46558541eb926c2ef52dd4d4594f1ab14d53624b
minkrov <minkrov@proton.me>
2026-03-05 14:39:10 +0100

    Help

--- Multiple choice quiz.py
+++ Multiple choice quiz.py
@@ -1,9 +1,1 @@
 ## Multiple choice quiz
-
-questions = {
-    "What color is an apple?": "red",
-    "What color is a banana?": "yellow",
-    "What color is the sky?": "blue",
-    "What color is the grass?": "green",
-}
-

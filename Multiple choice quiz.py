73732f193bf1064374ad6282aac3fcdf3059fd73
minkrov <minkrov@proton.me>
2026-03-05 08:10:49 +0100

    Questions - quiz

--- Multiple choice quiz.py
+++ Multiple choice quiz.py
@@ -1,1 +1,5 @@
 ## Multiple choice quiz
+
+questions = [
+    "What's a macbook?", "What's a iPad?", "What's a iPhone?", "What's a Apple Watch?", "What's a Apple TV?", "What's a Apple Music?", "What's a Apple Store?", "What's a Apple Support?", "What's a Apple Care?", "What's a Apple Watch?", "What's a Apple TV?", "What's a Apple Music?", "What's a Apple Store?", "What's a Apple Support?", "What's a Apple Care?",
+]
